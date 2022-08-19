b6f664b42055b0884124e0b0f02d0a2b0946814a
alexba6 <alexisbaylet6@gmail.com>
2022-08-19 13:53:29 +0200

    Update login page

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,17 +1,16 @@
 import { FunctionComponent, useContext } from 'react'
+import {Button, IconButton, Stack, TextField, Tooltip} from "@mui/material";
+import GoogleIcon from '@mui/icons-material/Google';
+import FacebookIcon from '@mui/icons-material/Facebook';
 
-import { Button } from '../Components/Button/Button'
 import { TextLink } from '../Components/Link/TextLink'
-
-import { GoogleIcon } from '../Icons/Form/Google'
-import { FacebookIcon } from '../Icons/Form/Facebook'
 import { CardForm, CardFormOrSeparator } from '../Components/Card/CardForm'
 import { HomeLogo } from '../Icons/HomeLogo'
 import { ContextAuthentication } from '../Context/ContextAuthentication'
 import { useHistory } from 'react-router-dom'
 import { RoutesPath } from '../Config/Routes'
 import { useFormValue } from '../Hooks/UseFormValue'
-import {TextField} from "@mui/material";
+import {CardHeader} from "../Components/Card/Card";
 
 export const LoginPage: FunctionComponent = () => {
 	const history = useHistory()
@@ -44,42 +43,60 @@ export const LoginPage: FunctionComponent = () => {
 
 	return (
 		<CardForm>
-			<div className="mv-2 flex flex-column flex-align-center">
-				<div className="flex flex-justify-center flex-align-center">
+			<CardHeader>
+				<Stack direction='column' justifyContent='space-between' alignItems='center'>
 					<div>
 						<HomeLogo />
 					</div>
-					<div className="mh-1">
+					<div>
 						<h1>Home</h1>
 					</div>
-				</div>
-				<div>
-					<p>
-						Connectez-vous à votre compte <strong>Home</strong>
-					</p>
-				</div>
-			</div>
+					<div>
+						<p>
+							Connectez-vous à votre compte <strong>Home</strong>
+						</p>
+					</div>
+				</Stack>
+			</CardHeader>
 			<div>
-				<TextField label="Email ou nom d'utilisateur" onChange={form.set.login} value={form.value.login} />
-				<TextField label="Mot de passe" type="password" onChange={form.set.password} value={form.value.password} />
+				<TextField
+					label="Email ou nom d'utilisateur"
+					onChange={form.set.login}
+					value={form.value.login}
+					margin='normal'
+					fullWidth/>
+				<TextField
+					label="Mot de passe"
+					type="password"
+					onChange={form.set.password}
+					value={form.value.password}
+					margin='normal'
+					fullWidth/>
 			</div>
 			<div className="flex flex-align-center flex-justify-end">
 				<TextLink onClick={() => {}}>Mot de passe oublié ?</TextLink>
 			</div>
 			<div className="mv-2 flex flex-column flex-justify-center">
-				<Button variant="primary" onClick={onLogin}>
+				<Button variant='outlined' color='primary' onClick={onLogin}>
 					Connexion
 				</Button>
 				<CardFormOrSeparator />
-				<div className="flex flex-align-center flex-justify-center margin">
-					<Button variant="outline-secondary" onClick={() => alert('Google')}>
-						<GoogleIcon />
-					</Button>
-					<div className="mh-2" />
-					<Button variant="outline-secondary" onClick={() => alert('Facebook')}>
-						<FacebookIcon />
-					</Button>
-				</div>
+				<Stack direction='row' justifyContent='center' alignItems='center'>
+					<div>
+						<Tooltip title='Connexion avec google'>
+							<IconButton>
+								<GoogleIcon/>
+							</IconButton>
+						</Tooltip>
+					</div>
+					<div>
+						<Tooltip title='Connexion avec facebook'>
+							<IconButton>
+								<FacebookIcon/>
+							</IconButton>
+						</Tooltip>
+					</div>
+				</Stack>
 			</div>
 		</CardForm>
 	)
